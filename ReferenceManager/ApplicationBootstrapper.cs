8d3a644a83ae3f5c9f9cdace872077d5d083d07e
forsvarir <forsvarir@blueyonder.co.uk>
2017-02-01 14:06:58 +0000

    Modified to use prism + unity to start application

---
+++ ReferenceManager/ApplicationBootstrapper.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using Microsoft.Practices.Unity;
+using Prism.Unity;
+using ReferenceManager.Views;
+
+namespace ReferenceManager
+{
+    public class ApplicationBootstrapper : UnityBootstrapper
+    {
+        protected override DependencyObject CreateShell()
+        {
+            return Container.Resolve<MainWindow>();
+        }
+
+        protected override void InitializeShell()
+        {
+            Application.Current.MainWindow.Show();
+        }
+
+        protected override void ConfigureContainer()
+        {
+            base.ConfigureContainer();
+        }
+    }
+}
--- ReferenceManager/Views/MainWindow.xaml.cs
+++ ReferenceManager/Views/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace ReferenceManager
+namespace ReferenceManager.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
